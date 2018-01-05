65f7018c568d6d3980fbd8b94cae30efe56e16a6
Mary Franks <maryfranks@gmail.com>
2018-01-05 08:16:15 -0500

    change hello world to use name prop

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import Hello from './App';
 
 ReactDOM.render(
-  <Hello />, document.getElementById('root')
+  <Hello name={"Nick"} />,
+  document.getElementById('root')
 );
