1a4418720af6b70b1a6431be4cbeb5f67b557bf9
Mary Franks <maryfranks@gmail.com>
2018-01-06 11:32:30 -0500

    add moodPoints state to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import React, {Component} from 'react';
 
 // define our Hello component
 class Hello extends Component {
+  //what should happen when the component is first created
+  constructor (props) {
+    super()
+    this.state = {
+      moodPoints: 1
+    }
+  }
   // what should the component render?
   render () {
     // make sure to return some UI
@@ -12,6 +19,7 @@ class Hello extends Component {
       <h1>Hello {this.props.name}!</h1>
       <h3>You are {this.props.age} years old.</h3>
       <p>You love: {this.props.animals[2]}</p>
+      <p>You are this happy: {this.state.moodPoints}</p>
       </div>
     )
   }
